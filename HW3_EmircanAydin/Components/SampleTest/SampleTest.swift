cac1cdda7441474f1135e0838db62e1c2398022a
emircanaydin <emircanaydin@menulux.com>
2021-10-08 23:59:23 +0300

    Components are added.

---
+++ HW3_EmircanAydin/Components/SampleTest/SampleTest.swift
@@ -0,0 +1,20 @@
+//
+//  SampleTest.swift
+//  ios_Week_3
+//
+//  Created by EmircanAydin on 08.10.2021.
+//
+
+import UIKit
+
+class SampleTest: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .cyan
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        backgroundColor = .darkGray
+    }
+}
